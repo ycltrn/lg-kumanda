e16910014e987331bb0c3b6b16ea07ceecb29837
ycltrn <34971494+ycltrn@users.noreply.github.com>
2026-03-23 01:37:17 +0300

    Add files via upload

--- proxy.py
+++ proxy.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.request, urllib.error, os
+import urllib.request, urllib.error, os, re
 
 TV = ["192.168.1.101"]
+SESSION = [""]
 PORT = 9090
 HTML_FILE = os.path.expanduser("~/lg_remote.html")
 
@@ -34,29 +35,46 @@ class H(BaseHTTPRequestHandler):
         n = int(self.headers.get("Content-Length",0))
         body = self.rfile.read(n)
         body_str = body.decode('utf-8', errors='ignore')
-        
-        # Pairing istekleri icin dogru endpoint sec
-        if 'AuthKeyReq' in body_str:
-            tv_path = "/udap/api/pairing"
-        elif 'AuthReq' in body_str or 'hello' in body_str:
+
+        # Endpoint sec
+        if 'AuthKeyReq' in body_str or 'AuthReq' in body_str or 'hello' in body_str:
             tv_path = "/udap/api/pairing"
         else:
             tv_path = "/udap/api/data"
-            
+
         url = "http://{}:8080{}".format(TV[0], tv_path)
-        print("-> {} {}".format(tv_path, body_str[:50]))
+        print("-> {} | session={}".format(tv_path, SESSION[0][:20] if SESSION[0] else "none"))
+
         try:
             r = urllib.request.Request(url, data=body, method="POST")
             r.add_header("Content-Type","application/atom+xml")
+            # Session key varsa gonder
+            if SESSION[0]:
+                r.add_header("Cookie", "session={}".format(SESSION[0]))
+
             with urllib.request.urlopen(r, timeout=5) as res:
                 data = res.read()
-                print("<-", data[:80])
+                # Session key yakala
+                cookie = res.headers.get("Set-Cookie","")
+                if cookie and "session=" in cookie:
+                    m = re.search(r'session=([^;]+)', cookie)
+                    if m:
+                        SESSION[0] = m.group(1)
+                        print("SESSION KEY:", SESSION[0][:20])
+                # XML'den session key yakala
+                data_str = data.decode('utf-8', errors='ignore')
+                m = re.search(r'<session>([^<]+)</session>', data_str)
+                if m:
+                    SESSION[0] = m.group(1)
+                    print("SESSION KEY XML:", SESSION[0][:20])
+                print("<-", data_str[:80])
+
             self.send_response(200); self.cors()
             self.send_header("Content-Type","application/atom+xml")
             self.end_headers(); self.wfile.write(data)
         except urllib.error.HTTPError as e:
             data = e.read()
-            print("<- ERR", e.code, data[:80])
+            print("<- ERR", e.code)
             self.send_response(e.code); self.cors()
             self.send_header("Content-Type","application/atom+xml")
             self.end_headers(); self.wfile.write(data)
